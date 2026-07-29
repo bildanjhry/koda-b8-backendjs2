45a28c88618f04cd1880da273f3c31de5f7b95ae
Bildan Jauhary <bildanjauhary@gmail.com>
2026-07-29 15:32:31 +0700

    feat: add models to handle storage

---
+++ server/src/models/auth_models.js
@@ -0,0 +1,60 @@
+import fs from "node:fs"
+
+export function create(data) {
+	let formated = []
+	try {
+		const date = new Date()
+		const newDate = `${date.getDate()} ${date.getMonth()} ${date.getFullYear()}, ${date.getHours()}:${date.getMinutes()}`
+
+		if (fs.existsSync("./server/src/models/users.json")) {
+			const file = fs.readFileSync("./server/src/models/users.json", 'utf-8')
+			formated = JSON.parse(file)
+			if (formated.find((item) => item.email === data.email)) {
+				throw new Error("Email already being used")
+			}
+		}
+		let users = []
+		users = [...formated,
+		{
+			id: formated.length + 1,
+			...data,
+			notes: [],
+			created_at: newDate,
+			updated_at: newDate
+		}]
+		fs.writeFileSync("./server/src/models/users.json", JSON.stringify(users))
+		return {
+			success: true, message: "Success Create Account", result: {
+				id: formated.length + 1,
+				name: data.name,
+				emai: data.email
+			}
+		}
+	} catch (err) {
+		console.error(err.message)
+		return { success: false, message: err.message, result: formated }
+	}
+}
+
+export function findUser(data) {
+	let formated = []
+	try {
+		if (!(fs.existsSync("./server/src/models/users.json"))) {
+			throw new Error("Users are empty")
+		}
+		const file = fs.readFileSync("./server/src/models/users.json", 'utf-8')
+		formated = JSON.parse(file)
+		if (formated.find((item) => item.email === data.email && item.email === data.password)) {
+			throw new Error("Email or password wrong")
+		}
+		return {
+			success: true, message: "Success Login", result: {
+				id: formated.length,
+				token:"Allow"
+			}
+		}
+	} catch (err) {
+		console.error(err.message)
+		return { success: false, message: err.message, result: formated }
+	}
+}
---
+++ server/src/models/users_models.js
@@ -0,0 +1,30 @@
+import fs from "node:fs"
+
+export function findUserDetail(id) {
+    let formated = []
+    try {
+        if (!(fs.existsSync("./server/src/models/users.json"))) {
+            throw new Error("Users are empty")
+        }
+        const file = fs.readFileSync("./server/src/models/users.json", 'utf-8')
+        formated = JSON.parse(file)
+        console.log(id)
+        let found = formated.filter((item) => item.id === parseInt(id))
+        console.log(formated)
+
+        if (found.length < 1) {
+            throw new Error("User not found")
+        }
+        return {
+            success: true, 
+            message: "Success Login", 
+            result:found
+        }
+    } catch (err) {
+        console.error(err.message)
+        return { 
+            success: false, 
+            message: err.message, 
+            result: formated }
+    }
+}

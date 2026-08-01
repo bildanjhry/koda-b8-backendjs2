15ad76d69024e6a85c48278bfb6d1e237d38ca37
Bildan Jauhary <bildanjauhary@gmail.com>
2026-08-01 21:57:17 +0700

    feat: delete models dir recursively

--- server/src/controllers/users_ctrl.js
+++ server/src/controllers/users_ctrl.js
@@ -6,23 +6,6 @@ import * as userService from "../services/users.services.js"
  * @param {import("express").Request} req 
  * @param {import("express").Response} res 
  */
-// export function getUserDetail(req, res){
-//     const id = req.params.id
-//     const response = findUserDetail(id)
-//     if(!response.success){
-//         res.status(constants.HTTP_STATUS_INTERNAL_SERVER_ERROR).json({
-//             success:false,
-//             message:response.message
-//         })
-//         return
-//     }
-//     res.status(constants.HTTP_STATUS_OK).json({
-//         success:true,
-//         message:response.message,
-//         results:response.result
-//     })
-// }
-
 
 export async function getUserDetail(req, res){
     try{
